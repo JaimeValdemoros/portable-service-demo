4bcbfd4d5c807b3944bc5ad055f58c76a96cff7d
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-09-06 20:57:44 +0000

    Run nix fmt

--- flake.nix
+++ flake.nix
@@ -4,24 +4,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         hello-service = pkgs.writeText "hello.service" (builtins.readFile ./hello.service);
-      in {
-        formatter = pkgs.nixfmt-tree;
+      in
+      {
         defaultPackage = pkgs.portableService {
           pname = "hello";
           version = "1.0";
           units = [ hello-service ];
           symlinks = [
-            { object = "${pkgs.hello}/bin/hello"; symlink = "/bin/hello"; }
+            {
+              object = "${pkgs.hello}/bin/hello";
+              symlink = "/bin/hello";
+            }
           ];
         };
-        devShell = with pkgs; mkShell {
-          buildInputs = [squashfsTools];
-        };
+        formatter = pkgs.nixfmt-tree;
+        devShell =
+          with pkgs;
+          mkShell {
+            buildInputs = [ squashfsTools ];
+          };
       }
     );
 }
