df88a484efd86a713f3d350c49a895dfd5e340f2
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-09-06 19:14:21 +0000

    Create example hello-world service

--- flake.nix
+++ flake.nix
@@ -8,8 +8,23 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        hello-service = pkgs.writeText "hello.service" ''
+          [Unit]
+          Description=Hello world
+
+          [Service]
+          Type=oneshot
+          ExecStart=${pkgs.hello}/bin/hello
+
+          [Install]
+          WantedBy=multi-user.target default.target
+        '';
       in {
-        defaultPackage = pkgs.hello;
+        defaultPackage = pkgs.portableService {
+          pname = "hello";
+          version = "1.0";
+          units = [ hello-service ];
+        };
         devShell = with pkgs; mkShell {
           buildInputs = [squashfsTools];
         };
