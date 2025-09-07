4d5db769148f1f9d4967fda5de533a985e8e54f9
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-09-07 01:34:27 +0000

    Interpolate cowsay path instead of symlinking to /bin

--- flake.nix
+++ flake.nix
@@ -14,26 +14,25 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        cowsay-service = {
+          name = "cowsay@.service";
+          outPath =
+            (pkgs.replaceVars (pkgs.writeText "cowsay.service" (builtins.readFile ./cowsay${"@"}.service)) {
+              cowsayExe = "${pkgs.cowsay}/bin/cowsay";
+            }).outPath;
+        };
+        cowsay-socket = {
+          name = "cowsay.socket";
+          outPath = "${./cowsay.socket}";
+        };
       in
       {
         defaultPackage = pkgs.portableService {
           pname = "cowsay";
           version = "1.0";
           units = [
-            {
-              name = "cowsay@.service";
-              outPath = (pkgs.writeText "cowsay.service" (builtins.readFile ./cowsay${"@"}.service)).outPath;
-            }
-            {
-              name = "cowsay.socket";
-              outPath = "${./cowsay.socket}";
-            }
-          ];
-          symlinks = [
-            {
-              object = "${pkgs.cowsay}/bin/cowsay";
-              symlink = "/bin/cowsay";
-            }
+            cowsay-service
+            cowsay-socket
           ];
         };
         formatter = pkgs.nixfmt-tree;
